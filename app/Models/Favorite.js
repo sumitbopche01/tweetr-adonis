11f62c21ec6c2045281ceb392d902ff571227d2f
codeitdown <sumitbopche01@gmail.com>
2020-06-06 12:58:48 +0530

    favorite to tweet, user relationship

--- app/Models/Favorite.js
+++ app/Models/Favorite.js
@@ -4,6 +4,14 @@
 const Model = use('Model')
 
 class Favorite extends Model {
+
+    tweet() {
+        return this.belongsTo('App/Models/Tweet')
+    }
+
+    user() {
+        return this.belongsTo('App/Models/User')
+    }
 }
 
 module.exports = Favorite
